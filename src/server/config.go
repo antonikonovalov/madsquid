e2a01148c3ebc2901f5fda2cae63809aca0db1cc
Dmitry Panov <reinventer@gmail.com>
2016-09-17 09:16:40 +0300

    fix address

--- src/server/config.go
+++ src/server/config.go
@@ -3,7 +3,7 @@ package server
 import "flag"
 
 var (
-	fAddr = flag.String("addr", "localhost:10443", "host:port for MadSquid")
+	fAddr = flag.String("addr", ":10443", "host:port for MadSquid")
 	fCert = flag.String("cert", "server.crt", "Certificate file")
 	fKey  = flag.String("keyfile", "server.key", "Key file")
 )
